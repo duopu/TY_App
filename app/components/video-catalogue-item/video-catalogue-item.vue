a22ccc0cc5b9537e6d6e63c14ace53c48f354237
fysimple <1548276041@qq.com>
2021-08-28 13:30:35 +0800

    视频目录

---
+++ app/components/video-catalogue-item/video-catalogue-item.vue
@@ -0,0 +1,34 @@
+<!-- 视频目录 -->
+<template>
+	<view class="catalogue-item">
+		<view class="flex-center-between root-item">
+			<text class="serial-number">1</text>
+			<text class="text flex-1 text-ellipsis">教师资格证笔试</text>
+			<view class="flex-center">
+				<image src="../../static/images/icons/icon-video.svg" class="image-video" mode="aspectFill"></image>
+				<text class="color-yellow">试看</text>
+			</view>
+		</view>
+		<view class="sub">
+			<view class="flex-center-between sub-item" v-for="(item,index) in ['','','','','','','','']" :key="index">
+				<text class="serial-number">1.1</text>
+				<text class="text flex-1 text-ellipsis">教师资格证笔试</text>
+				<view class="flex-center">
+					<image src="../../static/images/icons/icon-video.svg" class="image-video" mode="aspectFill"></image>
+					<text class="color-yellow">试看</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'video-catalogue-item',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less" src="./style.less" scoped></style>
