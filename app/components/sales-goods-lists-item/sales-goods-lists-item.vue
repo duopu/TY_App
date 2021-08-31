e02f44bb2be53f5bd53e1a1f971ceeff14cd7d52
fysimple <1548276041@qq.com>
2021-08-31 18:00:16 +0800

    活动弹窗和tabs

---
+++ app/components/sales-goods-lists-item/sales-goods-lists-item.vue
@@ -0,0 +1,51 @@
+<!-- 活动-分销大使-商品分销 -->
+<template>
+	<view class="sales-goods-lists-item">
+		<view class="item-top">
+			<image class="goods-image" src="../../static/images/other/demo.png" mode="aspectFill"></image>
+			<view class="right flex-column-between">
+				<view class="name">5天英语全能挑战</view>
+				<view class="flex-center-between">
+					<view class="">售价:¥500</view>
+					<view class="flex-center">
+						预计佣金:
+						<view class="price">
+							500
+							<text class="unit">金币</text>
+						</view>
+					</view>
+				</view>
+				<view class="flex-center process">
+					<view class="process-item">
+						<text class="number">1</text>
+						等待下单
+					</view>
+					<view class="process-item">
+						<text class="number">2</text>
+						等待下单
+					</view>
+					<view class="process-item">
+						<text class="number">3</text>
+						已返佣
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="item-bottom flex-center-end">
+			<button class="btn btn-border grey">取消分销</button>
+			<button class="btn btn-border black">重新申请</button>
+			<button class=" btn btn-block">查看详情</button>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'sales-goods-lists-item',
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less" src="./style.less"></style>
