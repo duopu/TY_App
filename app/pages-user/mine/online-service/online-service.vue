5dabbf400a2d596c0faacfe39508a6884a913c5f
fysimple <1548276041@qq.com>
2021-09-01 18:05:41 +0800

    简单的界面

---
+++ app/pages-user/mine/online-service/online-service.vue
@@ -0,0 +1,46 @@
+<!-- 在线客服 -->
+<template>
+	<view class="online">
+		<view class="flex-center row">
+			<image class="icons" src="../../../static/images/icons/icon-yellow-phone.svg" mode="aspectFill"></image>
+			<text>平台客服电话:0420-58475928582</text>
+		</view>
+		<view class="flex-center row">
+			<image class="icons" src="../../../static/images/icons/icon-yellow-qq.svg" mode="aspectFill"></image>
+			<text>QQ:194819491</text>
+		</view>
+		<view class="flex-center row">
+			<image class="icons" src="../../../static/images/icons/icon-yellow-email.svg" mode="aspectFill"></image>
+			<text>邮箱:tiejtlr@126.com</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+	.online{
+		padding: 40rpx 20rpx;
+		height: 100%;
+		font-size: 28rpx;
+		background-color: #FFFFFF;
+		overflow: hidden;
+		box-sizing: border-box;
+		.row{
+			margin-top: 40rpx;
+		}
+		.icons{
+			margin-right: 24rpx;
+			width: 48rpx;
+			height: 48rpx;
+		}
+	}
+</style>
---
+++ app/pages-user/mine/setting/setting.vue
@@ -0,0 +1,50 @@
+<!-- 系统设置 -->
+<template>
+	<view class="setting">
+		<view class="row flex-center-between">
+			<text>版本更新</text>
+			<image mode="aspectFill" class="icon-arrow" src="../../../static/images/icons/icon-arrow-right.svg"></image>
+		</view>
+		<view class="row flex-center-between">
+			<text>联系我们</text>
+			<image mode="aspectFill" class="icon-arrow" src="../../../static/images/icons/icon-arrow-right.svg"></image>
+		</view>
+		<view class="row flex-center-between">
+			<text>关于我们</text>
+			<image mode="aspectFill" class="icon-arrow" src="../../../static/images/icons/icon-arrow-right.svg"></image>
+		</view>
+		<view class="row flex-center-between">
+			<text>意见反馈</text>
+			<image mode="aspectFill" class="icon-arrow" src="../../../static/images/icons/icon-arrow-right.svg"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+.setting{
+	height: 100%;
+	padding: 40rpx 20rpx 40rpx 30rpx;
+	background-color: #FFFFFF;
+	overflow: hidden;
+	box-sizing: border-box;
+	.row{
+		padding: 42rpx 0;
+		font-size: 28rpx;
+		font-weight: bold;
+	}
+	.icon-arrow{
+		width: 48rpx;
+		height: 48rpx;
+	}
+}
+</style>
