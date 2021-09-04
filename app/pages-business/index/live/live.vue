fbe4c976b94e86daece54b867191b82aef3f1f49
fysimple <1548276041@qq.com>
2021-09-04 23:57:56 +0800

    商家直播相关（完）+ 我的权益中心（完）

---
+++ app/pages-business/index/live/live.vue
@@ -0,0 +1,28 @@
+<!-- 直播 -->
+<template>
+	<scroll-view scroll-y="true" class="live">
+		<view class="title text-bold">历史直播</view>
+		<view class="live-lists">
+			<view class="item flex-column" v-for="(item,index) in ['','','','','']" :key="index">
+				<image class="image" src="../../../static/images/other/good_banner.png" mode="aspectFill"></image>
+				<view class="flex-center-between bottom">
+					<view class="text-ellipsis flex">
+						<text class="text-bold">20200414</text>
+						<text class="color-6">直播</text>
+					</view>
+					<view class="time">1小时</view>
+				</view>
+			</view>
+		</view>
+	</scroll-view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less" src="./style.less"></style>
---
+++ app/pages-business/my/benefit/benefit.vue
@@ -0,0 +1,52 @@
+<!-- 权益中心 -->
+<template>
+	<view class="benfit">
+		<!-- 账户 -->
+		<view class="box flex-column-center account">
+			<image class="avatar-image" src="../../../static/images/other/girl.png" mode="aspectFill"></image>
+			<view class="text-bold font-large">
+				商家A
+			</view>
+			<view class="level">
+				LV1
+			</view>
+		</view>
+		<!-- 升级规则 -->
+		<view class="box rule">
+			<view class="text-bold font-large">升级规则</view>
+			<view class="lists">
+				<view class="item flex-center-between" v-for="(item,index) in ['','','','']" :key="index">
+					<image class="image" src="../../../static/images/my/vip.png" mode="widthFix"></image>
+					<view class="flex-1 right">
+						<view class="text-bold font-large">LV3</view>
+						<view class="font-24">总营业额达<text class="color-red">1</text>元</view>
+					</view>
+				</view>
+				
+			</view>
+		</view>
+		<!-- 权益内容 -->
+		<view class="box rule">
+			<view class="text-bold font-large">权益内容</view>
+			<view class="lists">
+				<view class="item flex-center-between" v-for="(item,index) in ['','','','']" :key="index">
+					<image class="image" src="../../../static/images/my/vip.png" mode="widthFix"></image>
+					<view class="flex-1 right">
+						<view class="text-bold font-large">LV3</view>
+						<view class="font-24">xxxxxxxxx</view>
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style lang="less" src="./style.less"></style>
