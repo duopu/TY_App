4d564011544ba6461c9995f104abf48bae2de72f
fysimple <1548276041@qq.com>
2021-09-06 15:41:09 +0800

    退款详情(完)+入驻首页(完)+合作(完)

---
+++ app/pages-user/mine/check-in-person/check-in-person.vue
@@ -0,0 +1,34 @@
+<!-- 个人入驻 -->
+<template>
+	<view class="check-in-person">
+		<!-- 进度 -->
+		<view class="flex-center-between process-lists">
+			<view class="item flex-column-center">
+				<view class="number"></view>
+				<view class="text">签订入驻协议</view>
+			</view>
+		</view>
+		<!-- 内容 -->
+		<scroll-view scroll-y="true" class="check-in-form">
+			
+		</scroll-view>
+		<!-- 底部 -->
+		<view class="flex-center-center bottom">
+			<button class="btn">下一步</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less" src="./style.less">
+
+</style>
