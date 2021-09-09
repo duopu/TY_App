58d510b928d3672dd6dfc31298e2ce61197e1bba
fysimple <1548276041@qq.com>
2021-09-09 18:00:51 +0800

    店铺详情（未）

--- app/pages-user/index/store-details/store-details.vue
+++ app/pages-user/index/store-details/store-details.vue
@@ -1,13 +1,20 @@
 <!-- 店铺详情 -->
 <template>
 	<view class="store flex-column">
-		<!-- 内容 -->
-		<tab-store></tab-store>
+		<!-- 头部 -->
+		<view class="flex-center-between store-top">
+			<image @click="goBack" class="icon-back" src="../../../static/images/icons/icon-back.svg" mode="aspectFill"></image>
+			<custom-search v-if="tabbarIndex === 1" placeholder="搜索" @search="getSearchInput"></custom-search>
+		</view>
+		<!-- 店铺-->
+		<tab-store v-if="tabbarIndex === 0"></tab-store>
+		<!-- 商品 -->
+		<tab-goods v-else-if="tabbarIndex === 1"></tab-goods>
 		<!-- 底部 tabbar -->
 		<view class="store-bottom flex-center">
 			<view class="item flex flex-column-center" v-for="(item, index) in tabbarData" :key="index" @click="changeTabsContent(index)">
-				<image class="image" :src="tabsIndex === index ? item.iconSelectPath : item.iconPath" mode="aspectFill"></image>
-				<text :class="{ on: tabsIndex === index }">{{ item.text }}</text>
+				<image class="image" :src="tabbarIndex === index ? item.iconSelectPath : item.iconPath" mode="aspectFill"></image>
+				<text :class="{ on: tabbarIndex === index }">{{ item.text }}</text>
 			</view>
 		</view>
 	</view>
@@ -15,13 +22,15 @@
 
 <script>
 import TabStore from './tab-store.vue';
+import TabGoods from './tab-goods.vue';
 export default {
 	components: {
-		TabStore
+		TabStore,
+		TabGoods
 	},
 	data() {
 		return {
-			tabsIndex: 0,
+			tabbarIndex: 0,
 			tabbarData: [
 				{
 					iconPath: '../../../static/images/tabbars/store-tabbar-store.png',
@@ -42,12 +51,15 @@ export default {
 		};
 	},
 	methods: {
-		/**
-		 * 切换content
-		 * 点击客服进行跳转
-		 * */
+		// 切换content，点击客服进行跳转
 		changeTabsContent(index) {
-			if (index < 2) this.tabsIndex = index;
+			if (index < 2) this.tabbarIndex = index;
+		},
+		//  返回上一级
+		goBack() {
+			uni.navigateBack({
+				delta: -1
+			});
 		}
 	}
 };
---
+++ app/pages-user/index/store-details/tab-common.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="classify-details">
+		<view class="flex-center filter">
+			<view class="filter-item flex-1 flex-center-center" @click="openPopup('sortPopup')">
+				<text>综合排序</text>
+				<image class="icon" src="../../../static/images/icons/icon-collapse-arrow.svg" mode="aspectFill"></image>
+			</view>
+			<view class="filter-item flex-1 flex-center-center" @click="openPopup('cloudPopup')">
+				<text>云计算</text>
+				<image class="icon" src="../../../static/images/icons/icon-collapse-arrow.svg" mode="aspectFill"></image>
+			</view>
+			<view class="filter-item flex-1 flex-center-center" @click="openPopup('filterPopup')">
+				<text>筛选</text>
+				<image class="icon" src="../../../static/images/icons/icon-collapse-arrow.svg" mode="aspectFill"></image>
+			</view>
+		</view>
+		<my-scroll-view ref="list" class="classify-details-lists" :pageSize="queryParams.size" @loadData="onLoadData">
+			<template v-slot:list="slotProps">
+				<course-lists-item v-for="(item, index) in slotProps.list" :key="index" :data="item" @itemClick="goodsItemClick(item)"></course-lists-item>
+			</template>
+		</my-scroll-view>
+		<!-- 综合排序 -->
+		<classify-sort-popup :current-index="sortIndex" ref="sortPopup"></classify-sort-popup>
+		<!-- 分类 -->
+		<classify-cloud-popup ref="cloudPopup"></classify-cloud-popup>
+		<!-- 筛选 -->
+		<classify-filter-popup ref="filterPopup"></classify-filter-popup>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'tab-common',
+	data() {
+		return {
+			slotProps: {
+				list: []
+			},
+			queryParams: {
+				size: 20
+			}
+		};
+	},
+	methods: {
+		// 点击商品跳转
+		goodsItemClick() {},
+		// 打开弹窗
+		openPopup(refName) {
+			this.$refs[refName].open();
+		},
+		// 加载数据
+		onLoadData() {}
+	}
+};
+</script>
+
+<style lang="less" src="./style.less"></style>
