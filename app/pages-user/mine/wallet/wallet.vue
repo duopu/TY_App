21ff8a63ae6a91c2489f22a9401aff25e2168af8
OF1526 <xiangjiayu@bm001.com>
2021-09-12 10:57:00 +0800

    🐞 fix:提现

--- app/pages-user/mine/wallet/wallet.vue
+++ app/pages-user/mine/wallet/wallet.vue
@@ -3,7 +3,7 @@
 	<view class="wallet">
 		<view class="wallet-top">
 			<view class="wallet-money text-bold">
-				<text>1000.00</text>
+				<text>{{goldCoin}}</text>
 				<text class="unit">金币</text>
 			</view>
 			<button class="btn text-bold">我要提现</button>
@@ -13,29 +13,85 @@
 		<!-- 金币变动明细 -->
 		<view class="wallet-details">
 			<view class="title text-bold">金币变动明细：</view>
-			<view class="list-item flex-center-between">
+			<view class="list-item flex-center-between" v-if="dataList" v-for="(item,index) in dataList" :key="index">
 				<view class="flex-column">
-					<view>提现</view>
-					<view class="color-9">2021-03-21 18:03:12</view>
+					<view>{{item.remark}}</view>
+					<view class="color-9">{{item.createTime}}</view>
 				</view>
-				<view class="money text-bold color-red">-500金币</view>
-			</view>
-			<view class="list-item flex-center-between">
-				<view class="flex-column">
-					<view>每日任务</view>
-					<view class="color-9">2021-03-21 18:03:12</view>
-				</view>
-				<view class="money text-bold">+500金币</view>
+				<view class="money text-bold" :class="item.type != 1  && 'color-red'">{{ item.type == 1 ? '+' : '-' }}{{item.gold}}金币</view>
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+let page = 1,size = 10,hasMoreData = false;
 export default {
 	data() {
-		return {};
-	}
+		return {
+      goldCoin:0,
+      dataList:[]
+    };
+	},
+  
+  onLoad(){
+    this._refresh()
+  },
+
+  onShow(){
+    this.queryInfo()
+  },
+
+  onPullDownRefresh() {
+      this._refresh()
+  },
+
+  onReachBottom(options) {
+    if (hasMoreData) {
+        this.queryList();
+    } else {
+        console.log("没有更多");
+    }
+  },
+
+  methods:{
+
+    // 查询用户信息(金币)
+    async queryInfo(){
+      const data = await this.$http.get('/user/queryInfo',{},true) || {}
+      this.goldCoin = data.goldCoin
+    },
+
+    _refresh(){
+        page = 1
+        size = 10
+        this.queryList();
+    },
+
+    // 列表
+    async queryList(){
+      const params = {
+        page,
+        size
+      }
+      const data = await this.$http.get('/userGold/queryPage',params,true)
+      let dataList = data.content || [];
+      if (dataList) {
+          uni.stopPullDownRefresh();
+          if (page == 1) {
+              this.dataList = dataList;
+          } else {
+              this.dataList = this.dataList.concat(dataList);
+          }
+          if (this.dataList.length < data.totalSize) {
+              hasMoreData = true;
+              page++;
+          } else {
+              hasMoreData = false;
+          }
+      }
+    }
+  }
 };
 </script>
 
