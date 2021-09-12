b9fc8677beebce07fa73bef90e4e3dcea524b506
dingle <18911755085@163.com>
2021-09-12 23:29:08 +0800

    改造消息列表

--- app/pages-business/message/index/index.vue
+++ app/pages-business/message/index/index.vue
@@ -1,36 +1,92 @@
 <!-- 消息 -->
 <template>
-	<tab-message-index v-if="!showAccount" @change="onChange"></tab-message-index>
-	<tab-switch-account v-else @change="onChange"></tab-switch-account>
+	<view class="message flex-column">
+		<!-- 头部 -->
+		<view class="message-top flex-center-between">
+			<view class="state flex-center">
+				<image mode="aspectFill" src="" class="icon-state"></image>
+				<text>在线</text>
+			</view>
+			<text class="title text-bold">消息</text>
+			<view class="right-text">
+				<image @click="()=>showPop=!showPop" class="icon-more-dot" src="" mode="aspectFill"></image>
+				<view class="dot-lists" v-show="showPop">
+					<view class="dot-lists-item" v-for="(item, index) in dotsData" :key="index">{{ item }}</view>
+				</view>
+			</view>
+		</view>
+		<!-- 列表 -->
+		<scroll-view scroll-y="true" class="message-content">
+			<view class="account"> 
+				当前登录账号：
+				<text class="color-yellow text-bold">管理员1</text>
+			</view>
+			<!-- 交易信息 -->
+			<view class="lists-item" @click="jump('trade')">
+				<image src="" mode="aspectFill" class="item-image"></image>
+				<view class="flex-column flex-1">
+					<view class="name text-bold">交易信息</view>
+					<view class="desc">有1个新订单产生</view>
+				</view>
+				<view class="flex-column item-right">
+					<view class="time">2012-20-12</view>
+					<view class="number">15</view>
+				</view>
+			</view>
+			<!-- 店铺信息 -->
+			<view class="lists-item">
+				<image src="" mode="aspectFill" class="item-image"></image>
+				<view class="flex-column flex-1">
+					<view class="name text-bold">店铺信息</view>
+					<view class="desc">有新的退款审批需处理</view>
+				</view>
+				<view class="flex-column item-right">
+					<view class="time">2012-20-12</view>
+					<view class="number">15</view>
+				</view>
+			</view>
+			<!-- 用户消息列表 --> 
+			<view class="lists-item" v-for="(item,index) in ['','','','','']" :key="index">
+				<image src="../../../static/images/other/girl.png" mode="aspectFill" class="item-image"></image>
+				<view class="flex-column flex-1">
+					<view class="name text-bold">用户A</view>
+					<view class="desc">好哒，亲～～</view>
+				</view>
+				<view class="flex-column item-right">
+					<view class="time text-bold">2012-20-12</view>
+					<view class="number">15</view>
+				</view>
+			</view>
+		</scroll-view>
+	</view>
 </template>
 
 <script>
-import TabMessageIndex from './tab-message.vue';
-import TabSwitchAccount from './tab-switch-account.vue';
 export default {
 	name: 'messageIndex',
 	components: {
-		TabMessageIndex,
-		TabSwitchAccount
 	},
 	data() {
 		return {
-			showAccount: false //false-默认消息界面 true-其他账号信息界面
+			showPop:false,
+			dotsData: ['全部标为已读', '删除全部会话', '聊天设置', '新消息通知设置']
 		};
 	},
-	onLoad(){
+	mounted(){
 
 	},
 	methods: {
-		// 切换页面
-		onChange() {
-			this.showAccount = !this.showAccount;
+		
+		
+		// 跳转页面 便利方法
+		jump(type){
+			if(type == 'trade'){
+				uni.navigateTo({
+					url:`/pages-business/message/trade/trade`
+				})
+			}
 		},
-		queryMessage(){
-			this.$http.get('/message/queryListByStoreId',{},false).then(res => {
-				console.log(res);
-			})
-		}
 	}
-};
+}; 
 </script>
+<style lang="less" src="./style.less"></style>
