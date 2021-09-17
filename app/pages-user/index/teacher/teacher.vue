3f911c324eba7ef0315f1dd0946fb1346d9f66d1
fysimple <1548276041@qq.com>
2021-09-17 09:53:27 +0800

    分销取消+退款+老师详情

--- app/pages-user/index/teacher/teacher.vue
+++ app/pages-user/index/teacher/teacher.vue
@@ -1,43 +1,35 @@
+<!-- 老师详情 -->
 <template>
-	<view>
-		
+	<scroll-view scroll-y="true" class="teacher-details">
 		<!-- 老师基本信息 -->
-		<teacher-lists-item :data="teacherVO"></teacher-lists-item>
-		
+		<teacher-lists-item class="box" :data="teacherVO"></teacher-lists-item>
+		<view class="title text-bold">教学课程</view>
 		<!-- 教学课程 -->
-		<course-lists-item
-		v-for="(item, index) in teacherVO.teacherGoodsList"
-		:key="`goods-${index}`" 
-		:data="item"></course-lists-item>
-	
-	</view>
+		<course-lists-item class="box" v-for="(item, index) in teacherVO.teacherGoodsList" :key="`goods-${index}`" :data="item"></course-lists-item>
+	</scroll-view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				teacherId: undefined,
-				teacherVO: {}
-			}
-		},
-		onLoad(option) {
-			this.teacherId = option.teacherId;
-			this.queryTeacherInfo();
-		},
-		methods: {
-			// 查询老师详情
-			queryTeacherInfo(){
-				this.$http
-					.get('/teacher/queryInfo', {teacherId:this.teacherId}, true)
-					.then(res => {
-						this.teacherVO = res;
-					});
-			}
+export default {
+	data() {
+		return {
+			teacherId: undefined,
+			teacherVO: {}
+		};
+	},
+	onLoad(option) {
+		this.teacherId = option.teacherId;
+		this.queryTeacherInfo();
+	},
+	methods: {
+		// 查询老师详情
+		queryTeacherInfo() {
+			this.$http.get('/teacher/queryInfo', { teacherId: this.teacherId }, true).then(res => {
+				this.teacherVO = res;
+			});
 		}
 	}
+};
 </script>
 
-<style>
-
-</style>
+<style lang="scss" src="./style.scss"></style>
