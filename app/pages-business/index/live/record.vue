a623657ee4d54c4d4d68f8b2554327a18ac4860e
dingle <18911755085@163.com>
2021-10-10 14:12:25 +0800

    完善直播回放页面

--- app/pages-business/index/live/record.vue
+++ app/pages-business/index/live/record.vue
@@ -2,8 +2,8 @@
 <template>
 	<scroll-view scroll-y="true" class="live record">
 		<!-- 记录 -->
-		<view class="record-lists-item" v-for="(item,index) in dataList" :key="item.liveId">
-			<image class="image" src="../../../static/images/other/good_banner.png" mode="aspectFill"></image>
+		<view class="record-lists-item" v-for="(item,index) in dataList" :key="item.liveId" >
+			<image class="image" :src="item.thumbnail" mode="aspectFill"></image>
 			<!-- 直播 -->
 			<view class="row flex-center-between" v-if="item.liveState < 2">
 				<view class="left flex-column flex-1">
@@ -14,19 +14,24 @@
 					</view>
 				</view>
 				<!-- 直播-live -->
-				<view class="right flex-center-center live"><image class="icons" src="../../../static/images/icons/icon-live-play.svg" mode="aspectFill"></image></view>
+				<view class="right flex-center-center live" @click="gotoLiveRoom(item.liveId)">
+					<image class="icons" src="../../../static/images/icons/icon-live-play.svg" mode="aspectFill"></image>
+				</view>
 			</view>
 			<!-- 回放 -->
-			<view class="row flex-center-between" v-else>
+			<view class="row flex-center-between" v-else >
 				<view class="left flex-column flex-1">
 					<view class="flex-center">
 						<text class="circle"></text>
 						<text class="text-bold">{{item.createTime}}</text>
 						<text class="color-6">{{ item.liveName }}</text>
 					</view>
-					<view class="time">【回放】{{item.createTime}}</view>
+					<view class="time">【回放】{{item.playbackTime}}</view>
 				</view>
-				<view class="right flex-center-center"><image class="icons" src="../../../static/images/icons/icon-live-playback.svg" mode="aspectFill"></image></view>
+				<view class="right flex-center-center live" @click="gotoLiveRoom(item.liveId)">
+					<image class="icons" src="../../../static/images/icons/icon-live-playback.svg" mode="aspectFill">		
+					</image>
+				</view>
 			</view>
 		</view>
 		<view @click="startLive" class="publishLive">发起直播</view>
@@ -41,6 +46,9 @@ export default {
 			dataList: []
 		};
 	},
+	onReady() {
+		this.queryPageByStore();
+	},
 	methods:{
 		startLive(){
 			uni.navigateTo({
@@ -48,9 +56,14 @@ export default {
 			})
 		},
 		queryPageByStore(){
-			this.$http.get('/live/queryPageByStore',{ page: 1, size: 10 }).then(res => {
-				console.log(res);
-				this.dataList = res;
+			this.$http.get('/live/queryPageByStore',{ page: 1, size: 1000 }).then(res => {
+				this.dataList = res.content;
+			})
+		},
+		// 跳转直播详情
+		gotoLiveRoom(liveId){
+			uni.navigateTo({
+				url:`/pages-business/index/room/room?liveId=${liveId}`
 			})
 		}
 	}
---
+++ app/pages-business/index/room/room.vue
@@ -0,0 +1,171 @@
+<!-- 直播间 -->
+<template>
+	<view class="page-wrapper live-room">
+		<view class="live-room-top">
+			<!-- 直播间信息 -->
+			<view class="flex-center-between row">
+				<view class="flex-center">
+					<image class="avatar-image" :src="liveInfo.storeAvatar" mode="aspectFill"></image>
+					<text>{{liveInfo.storeName}}</text>
+					<!-- <button class="btn-border yellow" @click="onCollect">{{liveInfo.storeCollectionCheck == 1 ? '未收藏' : '已收藏'}}</button> -->
+					<image class="icon-user" src="../../../static/images/icons/icon-user.svg" mode="aspectFill"></image>
+					<text>{{userCount}}</text>
+				</view>
+				<image src="../../../static/images/icons/icon-close.svg" mode="aspectFill" class="icon-close"
+					@click="closePage"></image>
+			</view>
+			<!-- 视频 直播 兼容-->
+			<video class="video-wrapper" :src="livePullUrl" :autoplay="true" controls></video>
+			<!-- 菜单 -->
+			<custom-horizontal-tabs :currentIndex="tabsIndex" :data="tabsData" @change="getCurrentIndex">
+			</custom-horizontal-tabs>
+		</view>
+		<!-- 评论 -->
+		<scroll-view :scroll-top="messageScrollTop" v-show="tabsIndex === 0" scroll-y="true" class="live-room-content">
+			<view class="chat-row" v-for="msg in messageList">
+				<text class="nickname">{{msg.userName}}:</text>
+				<view>{{msg.textElem.msg}}</view>
+			</view>
+		</scroll-view>
+		<!-- 简介 -->
+		<scroll-view v-show="tabsIndex === 1" scroll-y="true" class="live-room-content">
+			<view class="chat-row">
+				<text class="label">章节名称:</text>
+				<view>{{liveInfo.courseClassName}}</view>
+			</view>
+			<view class="chat-row">
+				<text class="label">直播名称:</text>
+				<view>{{liveInfo.liveName}}</view>
+			</view>
+
+			<view class="chat-row">
+				<text class="label">直播说明:</text>
+				<view>{{liveInfo.liveIntro}}</view>
+			</view>
+		</scroll-view>
+		<!-- 底部 输入框 -->
+		<view class="live-room-bottom flex-center">
+			<input class="input" placeholder-class="input-placeholder" type="text" v-model="messageText"
+				placeholder="快来参与互动吧" confirm-type="send" />
+			<!-- <image class="icon" src="../../../static/images/icons/icon-talk.svg" mode="aspectFill"></image> -->
+			<text @click="sendMessage">发送</text>
+			<image class="icon" src="../../../static/images/icons/icon-share.svg" mode="aspectFill"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+
+	export default {
+		data() {
+			return {
+				// 直播id
+				liveId: '',
+				// 直播详情
+				liveInfo:{},
+				tabsIndex: 0,
+				// 当前人数
+				userCount:0,
+				tabsData: ['评论', '简介'],
+				// 拉流地址
+				livePullUrl: '',
+				// 群id
+				groupId: "",
+				// 消息为
+				messageText: '',
+				// 消息列表
+				messageList: [],
+				messageScrollTop: 0,
+
+			};
+		},
+		onLoad(option) {
+			this.liveId = option.liveId;
+			// 监听群消息
+			uni.$on('AdvancedMsgListen', this.getNewMessage)
+		},
+		onReady() {
+			this.queryLiveDetail();
+		},
+		onUnload() {
+			uni.$off('AdvancedMsgListen', this.getNewMessage)
+		},
+		computed: {
+			
+		},
+		watch: {
+			messageList() {
+				this.$nextTick(() => {
+					this.messageScrollTop = this.messageList.length * 200
+				})
+			},
+			groupId() {
+				// 加入群聊
+				this.$tool.imTool.joinGroup(this.groupId);
+				// 获取群历史
+				this.getGroupHistoryMessageList();
+				// 获取群在线成员数量
+				this.getLiveRoomUserCount();
+			}
+		},
+		methods: {
+			// 切换 tab
+			getCurrentIndex(value) {
+				this.tabsIndex = value;
+			},
+			// 关闭页面
+			closePage() {
+				uni.navigateBack({})
+			},
+			// 查询直播详情
+			queryLiveDetail() {
+				this.$http.get('/live/queryDetail', {
+					liveId: this.liveId
+				}, true).then(res => {
+					this.liveInfo = res;
+					this.groupId = res.imGroupId;
+					this.livePullUrl = res.liveState == 2 ? res.liveVideoUrl : res.livePullUrl;
+				})
+			},
+			// 获取直播间人数
+			getLiveRoomUserCount(){
+				this.$tool.imTool.getGroupOnlineMemberCount(this.groupId).then(res=>{
+					
+				})
+			},
+			// 店铺收藏 
+			onCollect(){
+				this.$http.post('/store/collect',{storeId:this.liveInfo.storeId},true).then(res=>{
+					this.queryLiveDetail();
+				})
+			},
+			// 加载历史消息
+			getGroupHistoryMessageList() {
+				const oldMessage = this.messageList[0] || {}
+				this.$tool.imTool.getGroupHistoryMessageList(this.groupId, oldMessage.msgId).then(historys => {
+					this.messageList = historys.filter(msg => msg.elemType == 1).reverse()
+				})
+			},
+			// 收到新消息
+			getNewMessage(message) {
+				this.getLiveRoomUserCount();
+				if (message.type !== "onRecvNewMessage") return;
+				let msg = message.msg
+				if (msg.elemType == 1 && msg.groupId == this.groupId) {
+					this.messageList.push(msg)
+				}
+			},
+			// 发送消息
+			sendMessage() {
+				this.$tool.imTool.sendGroupTextMessage(this.messageText, this.groupId).then(msg => {
+					this.messageList.push(msg);
+					this.messageText = ''
+				})
+			}
+
+		}
+
+	};
+</script>
+
+<style lang="less" src="./style.less"></style>
