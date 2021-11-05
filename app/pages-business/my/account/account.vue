4db9d242d4f49569eae88ed01a7688c5badfc405
dingle <18911755085@163.com>
2021-11-05 13:14:19 +0800

    完成支付宝绑定

--- app/pages-business/my/account/account.vue
+++ app/pages-business/my/account/account.vue
@@ -24,80 +24,111 @@
 </template>
 
 <script>
+	const PPAliPay = uni.requireNativePlugin('PP-Alipay');
+
 	export default {
 		data() {
 			return {
+				user:getApp().globalData.user
 			};
 		},
-		onLoad(){
-			console.log('用户信息',getApp().globalData.user);
+		onLoad() {
+
 		},
-		computed:{
-			user(){
-				return getApp().globalData.user
-			},
-			wxBind(){
+		computed: {
+			wxBind() {
 				return !!this.user.openId
 			},
-			aliBind(){
+			aliBind() {
 				return !!this.user.aliPayId
 			}
 		},
-		methods:{
-			resetPassword(){
+		methods: {
+			resetPassword() {
 				uni.navigateTo({
-					url:'/pages/reset-password/reset-password?roleStatus=business'
+					url: '/pages/reset-password/reset-password?roleStatus=business'
 				})
 			},
-			bindWx(){
+			bindWx() {
 				console.log('绑定微信');
-				if(this.wxBind)return;
+				if (this.wxBind) return;
 				uni.login({
-				  provider: 'weixin',
-				  success:  ({authResult})=> {
-				    console.log('微信登录',authResult);
-					this.$http.post('/storeUser/bindOpenId',{sourceType:1,openId:authResult.openid},true).then(res=>{
-						this.$tool.saveUserStorage({...this.user,openId:res.openId});
-					}) 
-				  }
+					provider: 'weixin',
+					success: ({
+						authResult
+					}) => {
+						console.log('微信登录', authResult);
+						this.$http.post('/storeUser/bindOpenId', {
+							sourceType: 1,
+							openId: authResult.openid
+						}, true).then(res => {
+							this.refreshUser({
+								...this.user,
+								openId: res.openId
+							})
+						})
+					}
 				});
 			},
-			bindAliPay(){
+			bindAliPay() {
 				console.log('绑定支付宝');
-				uni.login({
-				  scopes: 'auth_base',
-				  success:  ({authResult})=> {
-				    console.log('支付宝',authResult);
-					// this.$http.post('/storeUser/bindOpenId',{sourceType:1,openId:authResult.openid},true).then(res=>{
-					// 	this.$tool.saveUserStorage({...this.user,openId:res.openId});
-					// }) 
-				  }
-				});
+				if (this.aliBind) return;
+				this.$http.get('/storeUser/getAliOauth', {}, true).then(res => {
+					const authInfo = res.content;
+					PPAliPay.login({
+						authInfo,
+						appScheme: 'tyjy'
+					}, result => {
+						console.log('返回结果', result);
+						if(result.code == 200){
+							const data = result.data
+							this.$http.post('/storeUser/bindOpenId', {
+								sourceType: 3,
+								openId: data.userId
+							}, true).then(res => {
+								this.refreshUser({
+									...this.user,
+									aliPayId: res.aliPayId
+								})
+							})
+						}else{
+							this.$tool.showToast(`支付宝授权失败,${result.msg}`)
+						}
+						//{"code":200,"data":{"alipayOpenId":"xx","authCode":"xx","resultCode":"xx","userId":"xxx"},"msg":"登录完成"}
+					});
+				})
+			},
+			refreshUser(user){
+				this.$tool.saveUserStorage(user);
+				this.user = user;
 			}
 		}
 	}
 </script>
 
 <style lang="less">
-.account{
-	padding-top: 20rpx;
-	font-size: 28rpx;
-	height: 100%;
-	background-color: #f4f5f9;
-	.row{
-		padding: 24rpx 20rpx 24rpx 40rpx;
-		margin-bottom: 8rpx;
-		background-color: #FFFFFF;
-		.icon-arrow{
-			margin-left: 16rpx;
-			width: 32rpx;
-			height: 32rpx;
-		}
-		.text{
-			flex: 1;
-			color: #999999;
-			text-align: right;
+	.account {
+		padding-top: 20rpx;
+		font-size: 28rpx;
+		height: 100%;
+		background-color: #f4f5f9;
+
+		.row {
+			padding: 24rpx 20rpx 24rpx 40rpx;
+			margin-bottom: 8rpx;
+			background-color: #FFFFFF;
+
+			.icon-arrow {
+				margin-left: 16rpx;
+				width: 32rpx;
+				height: 32rpx;
+			}
+
+			.text {
+				flex: 1;
+				color: #999999;
+				text-align: right;
+			}
 		}
 	}
-}
 </style>
